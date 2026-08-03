1de553b5f4546776f2a3ae9efa870bc48eac8eb3
emaanzeeshan <emoo.zeeshan@gmail.com>
2026-08-03 20:09:26 +0500

    Fix signup access bug

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname, { extensions: ['html'] }));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
